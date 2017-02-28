46b8bc35ff250436b99139168e4e9b7b22276e57
DragosC <dragosflorincostea@gmail.com>
2017-02-28 09:24:06 +0200

    ex 9

--- labtasks/androidstudio/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
+++ labtasks/androidstudio/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
@@ -9,10 +9,12 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.animation.AlphaAnimation;
 
 public class MainActivity extends AppCompatActivity {
 
     final private static long TRANSPARENCY_EFFECT_DURATION = 5000;
+
 
     private ButtonClickListener buttonClickListener = new ButtonClickListener();
 
@@ -25,8 +27,14 @@ public class MainActivity extends AppCompatActivity {
             greetingTextView.setAlpha(1);
 
             // TODO: exercise 9a
+            greetingTextView.setText(greetingTextView.getText().toString().replace("xxx", "\n"+userNameEditText.getText()));
+
 
             // TODO: exercise 9b
+            AlphaAnimation fadeEffect = new AlphaAnimation(1.0f, 0.0f);
+            fadeEffect.setDuration(TRANSPARENCY_EFFECT_DURATION);
+            fadeEffect.setFillAfter(true);
+            greetingTextView.setAnimation(fadeEffect);
         }
 
     }
